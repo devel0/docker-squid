a89ae86453c2a510bc751cd0d8fe48e4828d33c4
Lorenzo Delana <oss.devel@searchathing.com>
2018-03-09 17:25:04 +0100

    fixed squid startup

--- squid-entrypoint.sh
+++ squid-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-squid -z
+if [ ! -e /var/spool/squid/00 ]; then squid -z; fi
+
 squid
 
 logfile=/var/log/squid/access.log
 
 while true; do
-	if [ ! -e $logfile ]; then sleep 1; fi
+	if [ ! -e $logfile ]; then sleep 1; else break; fi
 done
 
 tail -f $logfile
