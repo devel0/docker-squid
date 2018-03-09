3f949ecea7a7ef2c8dd119f8de7d3d236d80f98a
Lorenzo Delana <oss.devel@searchathing.com>
2018-03-09 17:18:00 +0100

    wait squid to start then log access.log

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,3 @@ RUN apt-get install -y squid
 
 COPY squid.conf /etc/squid
 COPY squid-entrypoint.sh /entrypoint.d
-
-CMD tail -f /var/log/squid/access.log
--- squid-entrypoint.sh
+++ squid-entrypoint.sh
@@ -2,3 +2,11 @@
 
 squid -z
 squid
+
+logfile=/var/log/squid/access.log
+
+while true; do
+	if [ ! -e $logfile ]; then sleep 1; fi
+done
+
+tail -f $logfile
